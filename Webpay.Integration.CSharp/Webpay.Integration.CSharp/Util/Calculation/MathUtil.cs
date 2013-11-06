36e21c758d96dbbc86a1df204186a770411eaa54
unknown <simon.wiippola@sveaekonomi.se>
2013-11-06 17:00:16 +0100

    Added file that was left out in last commit.

---
+++ Webpay.Integration.CSharp/Webpay.Integration.CSharp/Util/Calculation/MathUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Webpay.Integration.CSharp.Util.Calculation
+{
+    public static class MathUtil
+    {
+        public static long ConvertFromDecimalToCentesimal(decimal value)
+        {
+            value = Math.Truncate(value * 10000) / 10000; //Remove insignificant decimals
+            value = BankersRound(value); //Bankers rounding to two decimals
+            value = value * 100; //Convert to centesimal value (kr -> öre, eur -> cent, and so on)
+            return Convert.ToInt64(value); //Return as long
+        }
+
+        public static decimal BankersRound(decimal value)
+        {
+            return Math.Round(value, 2, MidpointRounding.ToEven);
+        }
+
+        public static decimal ReverseVatRate(decimal rate)
+        {
+            return (1 - (1 / (1 + rate / 100)));
+        }
+    }
+}

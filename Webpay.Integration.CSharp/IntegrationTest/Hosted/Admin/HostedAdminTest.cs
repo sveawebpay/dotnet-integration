2173a5ac2fadf0532055e8b25aed02509ac12000
Daniel Brolund <daniel.brolund@sveaekonomi.se>
2015-04-20 16:32:31 +0200

    Moved files from test projects

--- Webpay.Integration.CSharp/IntegrationTest/Hosted/Admin/HostedAdminTest.cs
+++ Webpay.Integration.CSharp/IntegrationTest/Hosted/Admin/HostedAdminTest.cs
@@ -4,13 +4,11 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Web;
-using System.Xml;
 using NUnit.Framework;
 using Webpay.Integration.CSharp.Config;
 using Webpay.Integration.CSharp.Hosted.Helper;
 using Webpay.Integration.CSharp.Order.Create;
 using Webpay.Integration.CSharp.Util.Constant;
-using Webpay.Integration.CSharp.Util.Security;
 using Webpay.Integration.CSharp.Util.Testing;
 using Webpay.Integration.CSharp.WebpayWS;
 using Webpay.Integration.CSharp.Webservice.Payment;
@@ -156,157 +154,6 @@ namespace Webpay.Integration.CSharp.IntegrationTest.Hosted.Admin
             var mac = nameValueCollection["mac"];
 
             return new PaymentResponse(messageBase64, mac, merchantId);
-        }
-    }
-
-    public class Confirm
-    {
-        public DateTime CaptureDate { get; private set; }
-        public int TransactionId { get; private set; }
-
-        public Confirm(int transactionId, DateTime captureDate)
-        {
-            TransactionId = transactionId;
-            CaptureDate = captureDate;
-        }
-    }
-
-    public class CancelRecurSubscription
-    {
-        public string SubscriptionId { get; private set; }
-
-        public CancelRecurSubscription(string subscriptionId)
-        {
-            SubscriptionId = subscriptionId;
-        }
-    }
-
-    public class PaymentResponse
-    {
-        public string MerchantId { get; private set; }
-        public string Mac { get; private set; }
-        public string MessageBase64 { get; private set; }
-        public string Message { get; private set; }
-        public XmlDocument MessageXmlDoc { get; private set; }
-        public long TransactionId { get; private set; }
-
-        public string SubscriptionId
-        {
-            get { return MessageXmlDoc.SelectSingleNode("//subscriptionid").InnerText; }
-        }
-
-        public PaymentResponse(string messageBase64, string mac, string merchantId)
-        {
-            Mac = mac;
-            MessageBase64 = messageBase64;
-            MerchantId = merchantId;
-
-            Message = Base64Util.DecodeBase64String(messageBase64);
-            MessageXmlDoc = new XmlDocument();
-            MessageXmlDoc.LoadXml(Message);
-
-            TransactionId = long.Parse(MessageXmlDoc.SelectSingleNode("//transaction").Attributes["id"].Value);
-        }
-
-    }
-
-    public class Annul
-    {
-        public long TransactionId { get; private set; }
-
-        public Annul(long transactionId)
-        {
-            TransactionId = transactionId;
-        }
-    }
-
-    public class WebpayAdmin
-    {
-        public static HostedAdmin Hosted(IConfigurationProvider configurationProvider, string merchantId, CountryCode countryCode)
-        {
-            return new HostedAdmin(configurationProvider, merchantId, countryCode);
-        }
-    }
-
-    public class HostedAdmin
-    {
-        public IConfigurationProvider ConfigurationProvider { get; private set; }
-        public string MerchantId { get; private set; }
-        public CountryCode CountryCode { get; private set; }
-
-        public HostedAdmin(IConfigurationProvider configurationProvider, string merchantId, CountryCode countryCode)
-        {
-            ConfigurationProvider = configurationProvider;
-            MerchantId = merchantId;
-            CountryCode = countryCode;
-        }
-
-        public PreparedHostedAdminRequest Annul(Annul annul)
-        {
-            var xml = string.Format(@"<?xml version=""1.0"" encoding=""UTF-8""?>
-                <annul>
-                <transactionid>{0}</transactionid>
-                </annul>", annul.TransactionId);
-
-            return new PreparedHostedAdminRequest(xml, CountryCode, MerchantId, ConfigurationProvider, "/annul");
-        }
-
-        public PreparedHostedAdminRequest CancelRecurSubscription(CancelRecurSubscription cancelRecurSubscription)
-        {
-            var xml = string.Format(@"<?xml version=""1.0"" encoding=""UTF-8""?>
-                <cancelrecursubscription>
-                <subscriptionid>{0}</subscriptionid>
-                </cancelrecursubscription>", cancelRecurSubscription.SubscriptionId);
-
-            return new PreparedHostedAdminRequest(xml, CountryCode, MerchantId, ConfigurationProvider, "/cancelrecursubscription");
-        }
-
-        public PreparedHostedAdminRequest Confirm(Confirm confirm)
-        {
-            var xml = string.Format(@"<?xml version=""1.0"" encoding=""UTF-8""?>
-                <confirm>
-                <transactionid>{0}</transactionid>
-                <capturedate>{1}</capturedate>
-                </confirm>", confirm.TransactionId, confirm.CaptureDate.ToString("yyyy-MM-dd") );
-
-            return new PreparedHostedAdminRequest(xml, CountryCode, MerchantId, ConfigurationProvider, "/confirm");
-        }
-    }
-
-    public class PreparedHostedAdminRequest
-    {
-        public readonly string Xml;
-        public readonly CountryCode CountryCode;
-        public readonly string MerchantId;
-        public readonly IConfigurationProvider ConfigurationProvider;
-        public readonly string ServicePath;
-
-        public PreparedHostedAdminRequest(string xml, CountryCode countryCode, string merchantId, IConfigurationProvider configurationProvider, string servicePath)
-        {
-            Xml = xml;
-            CountryCode = countryCode;
-            MerchantId = merchantId;
-            ConfigurationProvider = configurationProvider;
-            ServicePath = servicePath;
-        }
-
-        public HostedAdminResponse DoRequest()
-        {
-            return HostedAdminRequest.HostedAdminCall(GetEndPointBase(), Request());
-        }
-
-        private string GetEndPointBase()
-        {
-            var endPoint = ConfigurationProvider.GetEndPoint(PaymentType.HOSTED);
-            var baseUrl = endPoint.Replace("/payment", "");
-
-            var targetAddress = baseUrl + "/rest" + ServicePath;
-            return targetAddress;
-        }
-
-        public HostedAdminRequest Request()
-        {
-            return new HostedAdminRequest(Xml, ConfigurationProvider.GetSecretWord(PaymentType.HOSTED, CountryCode), MerchantId);
         }
     }
 }

670d35088e2147ef76fd3c701fff3c420bf353e6
Daniel Brolund <daniel.brolund@sveaekonomi.se>
2015-04-21 14:56:39 +0200

    Added some doc.

--- Webpay.Integration.CSharp/Webpay.Integration.CSharp/Hosted/Admin/WebpayAdmin.cs
+++ Webpay.Integration.CSharp/Webpay.Integration.CSharp/Hosted/Admin/WebpayAdmin.cs
@@ -3,8 +3,16 @@ using Webpay.Integration.CSharp.Util.Constant;
 
 namespace Webpay.Integration.CSharp.Hosted.Admin
 {
+    /// <summary>
+    /// Admin root. If you are looking for invoice and paymentplan administrative functionality,
+    /// please use the SOAP/WSDL integrations.
+    /// </summary>
     public class WebpayAdmin
     {
+        /// <summary>
+        /// Provides the Admin API for the so called Hosted services, meaning the API that integrates
+        /// administration for bank, card, and similar payment services.
+        /// </summary>
         public static HostedAdmin Hosted(IConfigurationProvider configurationProvider, string merchantId, CountryCode countryCode)
         {
             return new HostedAdmin(configurationProvider, merchantId, countryCode);

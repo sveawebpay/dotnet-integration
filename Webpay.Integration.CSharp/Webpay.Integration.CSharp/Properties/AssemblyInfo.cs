223ef1b4075d314f6913a08d122653731f5a5f19
fre-sund <fredrik.sundell@sveaekonomi.se>
2019-04-05 08:34:33 +0200

    Bump version

--- Webpay.Integration.CSharp/Webpay.Integration.CSharp/Properties/AssemblyInfo.cs
+++ Webpay.Integration.CSharp/Webpay.Integration.CSharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Svea Ekonomi AB")]
 [assembly: AssemblyProduct("Webpay.Integration.CSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]

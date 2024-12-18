9cff9b26c7c23b497273edc511ac7db389e21af3
ornfelt <jonasornfelt@outlook.com>
2024-12-18 18:15:15 +0100

    remove unncessary ngrok files

--- Webpay.Integration/Sample.AspNetCore/ngrok-sample.bat
+++
@@ -1,1 +0,0 @@
-ngrok http https://localhost:44345 --subdomain=svea-sample --host-header=localhost:44345
--- Webpay.Integration/Sample.AspNetCore/ngrok-sample_2.bat
+++
@@ -1,1 +0,0 @@
-ngrok http https://localhost:44345 --subdomain=svea2-sample --host-header=localhost:44345

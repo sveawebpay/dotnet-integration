319c2ed5e963f49f67be5339c3806133af517926
Adil Irshad <adil.irshad@sveaekonomi.se>
2025-01-15 13:11:15 +0100

    Integration Package: Cannot credit when having special characters in the description
    #286558

---
+++ Webpay.Integration.CSharp/Webpay.Integration.CSharp/Util/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Webpay.Integration.CSharp.Util
+{
+    public static class Extensions
+    {
+        public static string XmlEscape(this string unescaped)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode node = doc.CreateElement("root");
+            node.InnerText = unescaped;
+            return node.InnerXml;
+        }
+
+        public static string XmlUnescape(this string escaped)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode node = doc.CreateElement("root");
+            node.InnerXml = escaped;
+            return node.InnerText;
+        }
+    }
+}
